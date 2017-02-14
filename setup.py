0885eaf6f505c06b292bd372ad73a576fdf32684
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-14 07:06:14 -0500

    Fixed package version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
         install_requires = file.read().splitlines()
 
     setuptools.setup(name=package_name,
-                     version='1.0',
+                     version='0.1',
                      description=description,
                      long_description=long_description,
                      url='https://github.com/dnguyen0304/python-common.git',
