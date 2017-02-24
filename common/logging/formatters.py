091b989f2ddb8ca8a6638d95b1da3b28bd4c114a
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-24 16:15:40 -0500

    ME-477: Changed to format explicitly

--- common/logging/formatters.py
+++ common/logging/formatters.py
@@ -2,7 +2,6 @@
 
 import json
 import logging
-import re
 
 
 class JsonFormatter(logging.Formatter):
@@ -31,38 +30,12 @@ class JsonFormatter(logging.Formatter):
         utilities.UnstructuredDataLogger
         """
 
-        replacement_field_keys = self._parse_format(self._style._fmt)
-        format = [(key, '%(' + key + ')s') for key in replacement_field_keys]
+        log_record = json.loads(self._style._fmt, encoding='utf-8')
 
         try:
-            for item in record._extra.items():
-                setattr(record, *item)
-                format.append((item[0], '%(' + item[0] + ')s'))
+            log_record.update(record._extra)
         except AttributeError:
             pass
 
-        return json.dumps(dict(format)) % record.__dict__
-
-    @staticmethod
-    def _parse_format(format):
-
-        """
-        Extract all replacement field keys.
-
-        Only the percent style is supported.
-
-        Parameters
-        ----------
-        format : str
-            Percent-style format string.
-
-        Returns
-        -------
-        list
-            List of strings of the replacement field keys.
-        """
-
-        pattern = '%\((\w+)\)'
-        matches = re.findall(pattern=pattern, string=format)
-        return matches or list()
+        return json.dumps(log_record) % record.__dict__
 
--- common/logging/tests/test_formatters.py
+++ common/logging/tests/test_formatters.py
@@ -1,52 +1,45 @@
 # -*- coding: utf-8 -*-
 
+import json
 import logging
 
-from nose.tools import (assert_false,
-                        assert_in,
-                        assert_list_equal,
-                        assert_true)
+from nose.tools import assert_false, assert_in, assert_true
 
 from common.logging import formatters
 
 
 class TestJsonFormatter:
 
+    def __init__(self):
+        self.formatter = None
+        self.log_record = None
+
     def setup(self):
-        class LogRecord:
-            pass
+        fmt = json.dumps({'timestamp': '%(asctime)s',
+                          'level_name': '%(levelname)s',
+                          'message': '%(message)s'})
+        self.formatter = formatters.JsonFormatter(fmt=fmt)
+        self.log_record = logging.makeLogRecord(dict())
 
-        self.formatter = formatters.JsonFormatter(fmt=logging.BASIC_FORMAT)
-        self.log_record = LogRecord()
-
-        self.log_record.levelname = 'levelname'
-        self.log_record.name = 'name'
-        self.log_record.message = 'message'
+        self.log_record.levelname = 'level_name'
+        # Set the msg attribute because the message attribute is
+        # overwritten in format() by getMessage().
+        self.log_record.msg = 'message'
 
     def test_to_json(self):
-        output = self.formatter.formatMessage(record=self.log_record)
+        log_message = self.formatter.format(record=self.log_record)
 
-        assert_true(output.startswith('{'))
-        assert_true(output.endswith('}'))
-        assert_in('"levelname": "levelname"', output)
-        assert_in('"name": "name"', output)
-        assert_in('"message": "message"', output)
+        assert_true(log_message.startswith('{'))
+        assert_true(log_message.endswith('}'))
+        assert_in('"timestamp"', log_message)
+        assert_in('"level_name": "level_name"', log_message)
+        assert_in('"message": "message"', log_message)
 
     def test_to_json_does_not_raise_attribute_error(self):
         raised_error = False
         try:
-            self.formatter.formatMessage(record=self.log_record)
+            self.formatter.format(record=self.log_record)
         except AttributeError:
             raised_error = True
         assert_false(raised_error)
-
-    def test_parse_format(self):
-        expected = ['levelname', 'name', 'message']
-        output = self.formatter._parse_format(logging.BASIC_FORMAT)
-        assert_list_equal(output, expected)
-
-    def test_parse_format_no_replacement_fields(self):
-        expected = list()
-        output = self.formatter._parse_format('')
-        assert_list_equal(output, expected)
 
